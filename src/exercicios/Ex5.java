c4f6e9e1a29de64546081a1ed9bd038a029a09f4
Rafael Vieira <rovieira540@gmail.com>
2025-10-28 13:30:28 -0300

    Resolução do exercício 05

--- src/exercicios/Ex5.java
+++ src/exercicios/Ex5.java
@@ -2,17 +2,17 @@ package exercicios;
 
 import java.util.Scanner;
 
-public class Ex5 {
+public class ex05 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite sua primeira nota: ");
+        double nota1 = input.nextDouble();
 
-        /*Calcular a média de duas notas*/
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Digite a primeira nota: ");
-            double n1 = sc.nextDouble();
-            System.out.print("Digite a segunda nota: ");
-            double n2 = sc.nextDouble();
-            double media = (n1 + n2) / 2;
-            System.out.println("Média final: " + media);
+        System.out.println("Digite sua segunda nota: ");
+        double nota2 = input.nextDouble();
 
+        double media = (nota1 + nota2) / 2;
+
+        System.out.println("A média das suas notas é: " + media);
     }
 }
